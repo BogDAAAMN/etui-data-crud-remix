e535c65ba11715ba3f91af3c0f41c43850c9d065
Bogdan Covrig <bogcov@gmail.com>
2022-03-29 10:38:24 +0200

    Add tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,17 +2,17 @@ import { Link } from "remix";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
+    <div className="m-5">
+      <h1 className="text-xl">Welcome to Remix</h1>
+      <ul className="list-disc">
+        <li className="text-blue-600 underline">
           <Link
             to="/docs"
           >
             ETUI Documents
           </Link>
         </li>
-        <li>
+        <li className="text-blue-600 underline">
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
@@ -21,7 +21,7 @@ export default function Index() {
             15m Quickstart Blog Tutorial
           </a>
         </li>
-        <li>
+        <li className="text-blue-600 underline">
           <a
             target="_blank"
             href="https://remix.run/tutorials/jokes"
@@ -30,7 +30,7 @@ export default function Index() {
             Deep Dive Jokes App Tutorial
           </a>
         </li>
-        <li>
+        <li className="text-blue-600 underline">
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
             Remix Docs
           </a>
