7e64507f14669ac6f7f083e1ed096135503176f8
Bogdan Covrig <bogcov@gmail.com>
2022-03-29 10:46:56 +0200

    Add tailwindui

--- app/routes/docs/index.tsx
+++ app/routes/docs/index.tsx
@@ -11,6 +11,10 @@ type LoaderData = {
   docs: Array<Document>;
 };
 
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export const loader = async () => {
   return json<LoaderData>({
     docs: [
@@ -29,23 +33,115 @@ export default function Documents() {
   console.log(docs);
 
   return (
-    <main>
-      <h1>ETUI Documents</h1>
-
-      <ul>
-        {docs.map((document) => (
-          <li key={document.id}>
-            <Link
-              to={document.id}
-              className="text-blue-600 underline"
-            >
-              {document.title}
-            </Link>
-
-            : {document.desc}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-gray-900">ETUI Documents</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the documents available in the ETUI archive.
+          </p>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+          >
+            Add document
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle">
+            <div className="shadow-sm ring-1 ring-black ring-opacity-5">
+              <table className="min-w-full border-separate" style={{ borderSpacing: 0 }}>
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                    >
+                      Description
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Status
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                    >
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {docs.map((document, documentIdx) => (
+                    <tr key={document.id}>
+                      <td
+                        className={classNames(
+                          documentIdx !== docs.length - 1 ? 'border-b border-gray-200' : '',
+                          'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8'
+                        )}
+                      >
+                        {document.id}
+                      </td>
+                      <td
+                        className={classNames(
+                          documentIdx !== docs.length - 1 ? 'border-b border-gray-200' : '',
+                          'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell'
+                        )}
+                      >
+                        {document.title}
+                      </td>
+                      <td
+                        className={classNames(
+                          documentIdx !== docs.length - 1 ? 'border-b border-gray-200' : '',
+                          'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell'
+                        )}
+                      >
+                        {document.desc}
+                      </td>
+                      <td
+                        className={classNames(
+                          documentIdx !== docs.length - 1 ? 'border-b border-gray-200' : '',
+                          'whitespace-nowrap px-3 py-4 text-sm text-gray-500'
+                        )}
+                      >
+                        {document.status}
+                      </td>
+                      <td
+                        className={classNames(
+                          documentIdx !== docs.length - 1 ? 'border-b border-gray-200' : '',
+                          'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-6 lg:pr-8'
+                        )}
+                      >
+                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                          Edit<span className="sr-only">, {document.name}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
