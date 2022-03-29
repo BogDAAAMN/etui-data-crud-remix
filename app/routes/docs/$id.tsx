68e128a873610d6ff2db60e5127b0a92efc09fca
Bogdan Covrig <bogcov@gmail.com>
2022-03-29 10:28:48 +0200

    Add `/docs/$id` route

---
+++ app/routes/docs/$id.tsx
@@ -0,0 +1,9 @@
+export default function DocumentId() {
+    return (
+      <main className="mx-auto max-w-4xl">
+        <h1 className="my-6 border-b-2 text-center text-3xl">
+          Some document
+        </h1>
+      </main>
+    );
+  }
