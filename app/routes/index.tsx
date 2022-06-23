0bb97c27de71104939dc0897fba75633c9762728
Bogdan Covrig <bogcov@gmail.com>
2022-06-23 14:14:59 +0200

    Add redirect on index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,37 +1,10 @@
-import { Link } from "remix";
+import { LoaderFunction } from "remix";
+import { redirect } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  return redirect("/agreements");
+};
 
 export default function Index() {
-  return (
-    <div className="m-5">
-      <h1 className="text-xl">Welcome to Remix</h1>
-      <ul className="list-disc">
-        <li className="text-blue-600 underline">
-          <Link to="/docs">ETUI Agreements</Link>
-        </li>
-        <li className="text-blue-600 underline">
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li className="text-blue-600 underline">
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li className="text-blue-600 underline">
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
